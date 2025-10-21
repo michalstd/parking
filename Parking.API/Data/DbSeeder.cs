e26d5b67ecc2b1df84c05479f885cdb4d9715c98
michalstd <mobopev705@fixwap.com>
2025-10-21 13:21:20 +0200

    Added seeding and Database Context

---
+++ Parking.API/Data/DbSeeder.cs
@@ -0,0 +1,18 @@
+﻿using Parking.Api.Models;
+
+namespace Parking.Api.Data
+{
+    public static class DbSeeder
+    {
+        public static void Seed(ParkingContext ctx, int numberOfSpaces = 10)
+        {
+            if (ctx.ParkingSpaces.Any()) return;
+
+            for (int i = 1; i <= numberOfSpaces; i++)
+            {
+                ctx.ParkingSpaces.Add(new ParkingSpace { Id = i, IsOccupied = false });
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
---
+++ Parking.API/Data/ParkingContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parking.Api.Models;
+
+namespace Parking.Api.Data
+{
+    public class ParkingContext : DbContext
+    {
+        public ParkingContext(DbContextOptions<ParkingContext> options)
+            : base(options) { }
+
+        public DbSet<ParkingSpace> ParkingSpaces { get; set; }
+        public DbSet<ParkingRecord> ParkingRecords { get; set; }
+    }
+}
