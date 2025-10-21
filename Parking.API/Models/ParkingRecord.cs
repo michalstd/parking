bd2915b95421af557109ee218aa50e3bf8d8e79a
michalstd <mobopev705@fixwap.com>
2025-10-21 13:00:21 +0200

    Added ParkingRecord and ParkingSpace models

---
+++ Parking.API/Models/ParkingRecord.cs
@@ -0,0 +1,13 @@
+﻿namespace Parking.Api.Models
+{
+    public class ParkingRecord
+    {
+        public int Id { get; set; }
+        public string VehicleReg { get; set; } = null!;
+        public int VehicleType { get; set; }
+        public int SpaceNumber { get; set; }
+        public DateTime TimeIn { get; set; }
+        public DateTime? TimeOut { get; set; }
+        public double? Charge { get; set; }
+    }
+}
---
+++ Parking.API/Models/ParkingSpace.cs
@@ -0,0 +1,8 @@
+﻿namespace Parking.Api.Models
+{
+    public class ParkingSpace
+    {
+        public int Id { get; set; }
+        public bool IsOccupied { get; set; }
+    }
+}
